1072435f4e6db96becb7100e7af14d75673753e3
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-06-11 14:11:23 +0100

    Finally working, after applying lots of evil.

--- lib/Dist/Zilla/Plugin/AssertOS.pm
+++ lib/Dist/Zilla/Plugin/AssertOS.pm
@@ -4,6 +4,8 @@ package Dist::Zilla::Plugin::AssertOS;
 
 use Moose;
 with 'Dist::Zilla::Role::FileGatherer';
+with 'Dist::Zilla::Role::InstallTool';
+with 'Dist::Zilla::Role::MetaProvider';
 
 use Devel::CheckOS qw[list_platforms];
 use File::Spec;
@@ -16,6 +18,14 @@ has 'os' => (
     default => sub { [] },
     auto_deref => 1,
 );
+
+sub metadata {
+  return {
+    no_index => {
+      directory => [ 'inc' ],
+    }
+  };
+}
 
 sub gather_files {
   my $self = shift;
@@ -66,6 +76,17 @@ sub gather_files {
   return;
 }
 
+sub setup_installer {
+  my $self = shift;
+  my ($mfpl) = grep { $_->name eq 'Makefile.PL' } @{ $self->zilla->files };
+  return unless $mfpl;
+  my $content = qq{use lib 'inc';\nuse Devel::AssertOS qw[};
+  $content .= join ' ', $self->os;
+  $content .= "];\n";
+  $mfpl->content( $content . $mfpl->content );
+  return;
+}
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
@@ -79,17 +100,63 @@ Dist::Zilla::Plugin::AssertOS - Require that our distribution is running on a pa
 
 =head1 SYNOPSIS
 
+  # In dist.ini - It is important that AssertOS follows MakeMaker
+
+  [MakeMaker]
+
+  [AssertOS]
+  os = Linux
+  os = FreeBSD
+  os = cygwin
+
+The resultant distribution will die at C<Makefile.PL> unless the platform the code is running on is Linux, FreeBSD or Cygwin.
+
 =head1 DESCRIPTION
+
+Dist::Zilla::Plugin::AssertOS is a L<Dist::Zilla> plugin that integrates L<Devel::AssertOS> so that CPAN authors
+may easily stipulate which particular OS environments their distributions may be built and installed on.
+
+The author specifies which OS or OS families are supported. The necessary L<Devel::AssertOS> files are copied to the 
+C<inc/> directory and C<Makefile.PL> is mungled to include the necessary incantation.
+
+On the module user side, the bundled C<inc/> L<Devel::AssertOS> determines whether the current environment is 
+supported or not and will die accordingly.
+
+As this plugin mungles the C<Makefile.PL> it is imperative that it is specified in C<dist.ini> AFTER C<[MakeMaker]>.
+
+This plugin also automagically adds the C<no_index> metadata so that C<inc/> is excluded from PAUSE indexing. If 
+you use L<Dist::Zilla::Plugin::MetaNoIndex>, there may be conflicts.
 
 =head2 ATTRIBUTES
 
+=over
+
+=item C<os>
+
+Specify as many times as wanted the OS that you wish your distribution to work with. See L<Devel::AssertOS> and
+L<Devel::CheckOS> for what may be given.
+
+=back
+
 =head2 METHODS
+
+These are required by the roles that this plugin uses.
 
 =over
 
+=item C<mvp_multivalue_args>
+
 =item C<gather_files>
 
-=item C<mvp_multivalue_args>
+Required by L<Dist::Zilla::Role::FileGatherer>.
+
+=item C<setup_installer>
+
+Required by L<Dist::Zilla::Role::InstallTool>.
+
+=item C<metadata>
+
+Required by L<Dist::Zilla::Role::MetaProvider>.
 
 =back
 
@@ -98,6 +165,11 @@ Dist::Zilla::Plugin::AssertOS - Require that our distribution is running on a pa
 Chris C<BinGOs> Williams
 
 Based on L<use-devel-assertos> by David Cantrell
+
+=head1 KUDOS
+
+Thanks to Ricardo Signes, not only for L<Dist::Zilla>, but for explaining L<Dist::Zilla::Role::InstallTool>'s
+place in the build process. This made this plugin possible.
 
 =head1 LICENSE
 
@@ -114,4 +186,3 @@ L<Devel::AssertOS>
 L<Devel::CheckOS>
 
 =cut
-
